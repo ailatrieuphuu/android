16232a3829d4927d1d5a7e7506ffa6b9b5d87ae8
NgocTam <50764337+ngoctam2258@users.noreply.github.com>
2019-11-06 10:53:56 +0700

    TraLoiCauHoi

---
+++ app/src/main/java/com/cdth17pma/traloicauhoi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cdth17pma.traloicauhoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.traloi);
+    }
+}
